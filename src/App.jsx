94c743c277f7c9b58aeee9660d975ede747157d1
jav3to <javiplus.-@live.com.ar>
2022-03-01 15:49:36 -0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -108,24 +108,23 @@ function App() {
                     {" "}
                     Intenta otra vez !!!
                   </Text>
-                  <Box background="blackAlpha.500" w="20%" p={3} color="white">
-                    <Stack direction="row" alignItems="center" justify="center">
-                      <Text color="black">
-                        El pokemon era:<br></br>
-                        <Text textShadow="1px 3px 3px black" color="green.200">
-                          {pokemon.name}
-                        </Text>
+
+                  <Stack direction="row" alignItems="center" justify="center">
+                    <Text color="black">
+                      El pokemon era:<br></br>
+                      <Text textShadow="1px 3px 3px black" color="green.200">
+                        {pokemon.name}
                       </Text>
-                    </Stack>
-                    <Button
-                      size="sm"
-                      autoFocus
-                      class="nes-btn is-success"
-                      onClick={() => handleReset()}
-                    >
-                      Jugar de nuevo
-                    </Button>
-                  </Box>
+                    </Text>
+                  </Stack>
+                  <Button
+                    size="sm"
+                    autoFocus
+                    class="nes-btn is-success"
+                    onClick={() => handleReset()}
+                  >
+                    Jugar de nuevo
+                  </Button>
                 </Stack>
               ) : (
                 <Text></Text>
